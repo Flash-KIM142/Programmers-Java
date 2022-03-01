7e79c4dfabf1f9f8fd18b9de79dabd431dd70ac3
Flash-KIM142 <topqr123q@gmail.com>
2022-03-01 23:05:10 +0900

    경주로 건 ( 3차시도: 우선순위큐 )

--- src/RaceTrack.java
+++ src/RaceTrack.java
@@ -1,65 +1,71 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.io.*;
 
 public class RaceTrack {
-    static int[][][] price;
-    static int N;
-    static int[][] move = { {-1,0}, {1,0}, {0,-1}, {0,1} }; // 상하좌우
+    static int[][][] visited; // 4 방향별 방문 정보
+    static int[][] dir = { {-1,0}, {1,0}, {0,-1}, {0,1} }; // 상하좌우
 
     static int solution(int[][] board) {
-        N = board.length;
-        price = new int[4][N][N]; // 방향, 행, 열
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++) {
-                price[0][i][j] = Integer.MAX_VALUE; price[1][i][j] = Integer.MAX_VALUE;
-                price[2][i][j] = Integer.MAX_VALUE; price[3][i][j] = Integer.MAX_VALUE;
-            }
+        int n = board.length;
+        int answer = 0;
+        visited = new int[n][n][4];
+        PriorityQueue<Info> q = new PriorityQueue<>();
+
+        visited[0][0][0] = visited[0][0][1] = visited[0][0][2] = visited[0][0][3] = -1;
+        if(board[0][1]==0) {
+            q.add(new Info(0, 1, 3,1));
+            visited[0][1][0] = 1;
         }
-        Queue<int[]> q = new LinkedList<>();
-        if(board[1][0]==0){
-            price[1][1][0] = 100;
-            q.add(new int[]{1,0,1});
-        }
-        if(board[0][1]==0){
-            price[3][0][1] = 100;
-            q.add(new int[]{0,1,3});
+        if(board[1][0]==0) {
+            q.add(new Info(1, 0, 1,1));
+            visited[1][0][1] = 1;
         }
 
         while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int cRow = cur[0];  int cCol = cur[1];  int cDir = cur[2];
+            Info cur = q.poll();
+            if(cur.row==n-1 && cur.col==n-1){
+                answer = cur.price;
+                break;
+            }
 
-            for(int nDir=0; nDir<4; nDir++){
-                int nRow = cRow + move[nDir][0]; int nCol = cCol + move[nDir][1];
-                if(!isInRange(nRow, nCol) || board[nRow][nCol]==1)   continue;
-                if(cDir!=nDir) { // 방향이 달라질 경우
-                    if(cDir+nDir==1 || cDir+nDir==5)  continue; // 역방향으로 돌아가면 넘겨
-                    if(price[nDir][nRow][nCol]<price[cDir][cRow][cCol]+600) continue;
-                    q.add(new int[]{nRow,nCol,nDir});
-                    price[nDir][nRow][nCol] = price[cDir][cRow][cCol] + 600;
-                }
-                else{ // 직선으로 갈 경우
-                    if(price[nDir][nRow][nCol]<price[cDir][cRow][cCol]+100) continue;
-                    q.add(new int[]{nRow, nCol, nDir});
-                    price[nDir][nRow][nCol] = price[cDir][cRow][cCol] + 100;
-                }
+            for(int n_dir=0; n_dir<4; n_dir++){
+                int n_row = cur.row + dir[n_dir][0];
+                int n_col = cur.col + dir[n_dir][1];
+                int n_price = cur.price + ((cur.dir == n_dir) ? 1 : 6);
+
+                if(n_row<0 || n_row>=n || n_col<0 || n_col>=n || board[n_row][n_col]==1)  continue; // 범위 밖 OR 벽이면 패스
+                if(visited[n_row][n_col][n_dir]!=0 && visited[n_row][n_col][n_dir] < n_price)    continue; // 이미 방문했으면 패스
+
+                visited[n_row][n_col][n_dir] = n_price;
+                q.add(new Info(n_row,n_col,n_dir,n_price));
             }
         }
-        int result = Integer.MAX_VALUE;
-        for(int i=0; i<4; i++)
-            result = (result>price[i][N-1][N-1] ? price[i][N-1][N-1]:result);
-        return result;
+
+        return answer*100;
     }
 
-    static Boolean isInRange(int row, int col){
-        if(row<0 || row>=N || col<0 || col>=N)  return false;
-        return true;
+    static class Info implements Comparable<Info> {
+        int row;
+        int col;
+        int dir;
+        int price;
+
+        Info(int row, int col, int dir, int price){
+            this.row = row;
+            this.col = col;
+            this.dir = dir;
+            this.price = price;
+        }
+
+        @Override
+        public int compareTo(Info cmp){
+            return this.price - cmp.price;
+        }
     }
 
     public static void main(String args[]) throws IOException {
         BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[][] board = {{0,0,0,0,0,0},{0,1,1,1,1,0},{0,0,1,0,0,0},{1,0,0,1,0,1}, {0,1,0,0,0,1}, {0,0,0,0,0,0}};
+        int[][] board = {{0,0,0,0,0,0,0,1},{0,0,0,0,0,0,0,0},{0,0,0,0,0,1,0,0},{0,0,0,0,1,0,0,0},{0,0,0,1,0,0,0,1},{0,0,1,0,0,0,1,0},{0,1,0,0,0,1,0,0},{1,0,0,0,0,0,0,0}};
         bfw.write(String.valueOf(solution(board)));
         bfw.close();
     }
