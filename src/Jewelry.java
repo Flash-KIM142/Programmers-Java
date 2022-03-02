85f5bd0a9563c3f09d6172db533d5fbe6a49c42a
Flash-KIM142 <topqr123q@gmail.com>
2022-03-03 03:10:11 +0900

    보석 쇼핑 ( 3차시도: 이중 포인터 )

--- src/Jewelry.java
+++ src/Jewelry.java
@@ -10,12 +10,12 @@ public class Jewelry {
     static int[] solution(String[] gems) {
         getTypes(gems);
         int typeSize = types.size();
-        if(typeSize==1) return new int[]{1, 1};
+        if(typeSize==1) return new int[]{1, 1}; // 종류 하나밖에 없으면 바로 (1,1) 리턴
 
         int gemsLen = gems.length;
-        if(typeSize==gemsLen) return new int[]{1, gemsLen};
+        if(typeSize==gemsLen) return new int[]{1, gemsLen}; // 종류 수랑 전체 보석 개수 똑같으면 처음부터 끝까지 바로 반환
 
-        int start=0, end=0; // 정답
+        int start=0, end=0; // 정답용
         int left=0, right=0; // 검사용
         int len = Integer.MAX_VALUE;
 
@@ -24,19 +24,17 @@ public class Jewelry {
                 map.put(gems[left], map.get(gems[left])-1);
                 if(map.get(gems[left])==0)
                     map.remove(gems[left]);
-                left++;
+                left++; // 왼쪽 커서 옮겨주기
             }
-            else if(right==gemsLen) {
+            else if(right==gemsLen) // 끝까지 갔으면 종료
                 break;
-            }
-            else{
-                map.put(gems[right], map.getOrDefault(gems[right++], 0) + 1);
-            }
+            else
+                map.put(gems[right], map.getOrDefault(gems[right++], 0) + 1); // 오른쪽 커서 옮겨주기
 
-            if(right-left < len && map.size()==typeSize){
+            if(right-left < len && map.size()==typeSize){ // 새로운 구간 찾으면 갱신해주기
                 len = right - left;
-                start = left + 1;
-                end = right;
+                start = left + 1; // 0부터가 아니라 1부터니까 1 추가
+                end = right; // 오른쪽 커서는 이미 한 칸 커졌으니까 놔둠
             }
         }
 
