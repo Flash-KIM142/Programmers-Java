bb8d0dd1d983179ac57e565d5db72e7cd6681f5d
Flash-KIM142 <topqr123q@gmail.com>
2022-03-17 23:46:00 +0900

    주차 요금 계산 ( 2차시도: 해시맵 )

--- src/ParkingBill.java
+++ src/ParkingBill.java
@@ -1,99 +1,61 @@
-import java.io.*;
 import java.util.*;
 
 public class ParkingBill {
-    static int[] solution(int[] fees, String[] records) {
-        int defaultTime = fees[0];  int defaultBill = fees[1];
-        int unitTime = fees[2]; int unitBill = fees[3];
-        HashMap<String, Info> map = new HashMap<>();
+    static HashMap<Integer, Queue<String>> map = new HashMap<>();
+    static ArrayList<Integer> carList = new ArrayList<>();
 
+    static int[] solution(int[] fees, String[] records){
         for(String record: records){
             StringTokenizer stk = new StringTokenizer(record);
-            String[] time = stk.nextToken().split(":");
-            String carNum = stk.nextToken();
-            int hrs = Integer.parseInt(time[0]);
-            int mins = Integer.parseInt(time[1]);
-            Time tmp = new Time(hrs, mins);
+            String time = stk.nextToken();
+            int carNo = Integer.parseInt(stk.nextToken());
 
-            if(map.get(carNum)==null)
-                map.put(carNum, new Info(tmp));
-            else
-                map.get(carNum).record.add(tmp);
+            if(!map.containsKey(carNo)){
+                Queue<String> tmp = new LinkedList<>();
+                map.put(carNo, tmp);
+            }
+            map.get(carNo).add(time);
+            if(!carList.contains(carNo))    carList.add(carNo);
         }
+        Collections.sort(carList);
 
-        for(Info a: map.values()){
-            a.getTotalBill(defaultTime,defaultBill,unitTime,unitBill);
-        }
-        Object[] mapKey = map.keySet().toArray();
-        Arrays.sort(mapKey);
+        int[] answer = new int[carList.size()];
+        int idx = 0;
+        for(int num: carList){
+            Queue<String> tmp = map.get(num);
+            if(tmp.size()%2!=0) tmp.add("23:59");
+            int total = 0;
 
-        int[] answer = new int[map.size()];
-        int i = 0;
-        for(Object key: mapKey){
-            answer[i] = map.get(key).totalBill;
-            i++;
+            while(!tmp.isEmpty()){
+                String in = tmp.poll();
+                String out = tmp.poll();
+                total += timeLap(in, out);
+            }
+
+            if(total<=fees[0])  answer[idx++] = fees[1];
+            else{
+                int units = ((total - fees[0]) / fees[2]);
+                if(((total - fees[0]) % fees[2])!=0)    units += 1;
+                answer[idx++] = fees[1] + units * fees[3];
+            }
         }
         return answer;
     }
 
-    static class Info{
-        Queue<Time> record = new LinkedList<>();
-        int totalBill;
+    static Integer timeLap(String in, String out){
+        String[] split = in.split(":");
+        int inMins = Integer.parseInt(split[0])*60 + Integer.parseInt(split[1]);
 
-        Info(Time a){ record.add(a); }
+        split = out.split(":");
+        int outMins = Integer.parseInt(split[0])*60 + Integer.parseInt(split[1]);
 
-        void getTotalBill(int defaultTime, int defaultBill, int unitTime, int unitBill){
-            int result = 0;
-            int totalMins = 0;
-            Time before = new Time(0,0);
-            Time after = new Time(0,0);
-            while(!this.record.isEmpty()){
-                if(this.record.size()!=1)   before = this.record.poll();
-                else    break;
-                after = this.record.poll();
-                Time lap = after.getLap(before);
-                totalMins += lap.hrs*60 + lap.mins;
-            }
-
-            if(!this.record.isEmpty()){
-                Time last = this.record.poll();
-                Time lap = new Time(23, 59).getLap(last);
-                totalMins += lap.hrs*60 + lap.mins;
-            }
-
-            if(totalMins<=defaultTime)  result += defaultBill;
-            else {
-                result += defaultBill;
-                totalMins -= defaultTime;
-
-                int unitCnt = (totalMins % unitTime == 0 ? totalMins / unitTime : totalMins / unitTime + 1);
-                result += unitCnt * unitBill;
-            }
-            this.totalBill = result;
-        }
+        return outMins - inMins;
     }
 
-    static class Time{
-        int hrs;
-        int mins;
-
-        Time(int hrs, int mins){
-            this.hrs = hrs;
-            this.mins = mins;
-        }
-
-        Time getLap(Time before){
-            if(this.mins - before.mins >=0)    return new Time(this.hrs-before.hrs, this.mins- before.mins);
-            else    return new Time(this.hrs-before.hrs-1, this.mins- before.mins+60);
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
         int[] fees = { 180, 5000, 10, 600 };
         String[] records = { "05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT" };
         int[] answer = solution(fees, records);
-        for(int bill: answer)   bfw.write(bill + " ");
-        bfw.close();
+        for(int bill: answer) System.out.println(bill + " ");
     }
 }
