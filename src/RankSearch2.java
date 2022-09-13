b9ee1ff91d209fbbcdbb593b75bad75a16cb2dd7
Flash-KIM142 <topqr123q@gmail.com>
2022-09-13 10:49:05 +0900

    순위 검색 (3차 시도: 실패 - hashmap, 조합 with 재귀)

---
+++ src/RankSearch2.java
@@ -0,0 +1,16 @@
+public class RankSearch2 {
+
+    static int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String q = "- and - and - and - 150";
+        q = q.replaceAll(" and ", " ");
+        String[] ary = q.split(" ");
+        for (String s : ary) {
+            System.out.println(s);
+        }
+    }
+}
